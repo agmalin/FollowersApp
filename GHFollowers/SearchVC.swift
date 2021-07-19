a6589ed7ee871f00ccbd578debd92644653b4519
Sergey Agmalin <sergeyagmalin@gmail.com>
2021-07-19 14:51:13 -0400

    created view controllers, embedded them into navigation controllers. Added tabbar. Programmatic UI

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Sergey Agmalin on 2021-07-19.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
